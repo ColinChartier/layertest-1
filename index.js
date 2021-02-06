5264e91c9482c7cd5cc91aa77f52690fe2f03f47
Colin Chartier <colin@layerci.com>
2021-02-05 19:02:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const _ = require('koa-route');
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
+app.use(_.get('/redirect', ctx => {
   ctx.redirect('https://google.com/imghp');
-});
+}));
+app.use(_.get('/', ctx => {
+  ctx.body = 'hello';
+}));
 
 app.listen(3000);
